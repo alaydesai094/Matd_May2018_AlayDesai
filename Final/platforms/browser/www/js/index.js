5ac5336c03db16b422824e5a210733e71b29f5f2
alaydesai094 <alaydesai094@gmail.com>
2018-11-01 09:37:03 -0400

    open Database test

--- Final/platforms/browser/www/js/index.js
+++ Final/platforms/browser/www/js/index.js
@@ -1,11 +1,97 @@
+document.addEventListener("deviceready", connectToDatabase);
+
+
+var db = null; 
+db = window.openDatabase("superdb", "1.0", "Database for SuperHeros Rescue Agency app", 2 * 1024 * 1024);
+
+function connectToDatabase() {
+  console.log("device is ready - connecting to database");
+  // 2. open the database. The code is depends on your platform!
+  if (window.cordova.platformId === 'browser') {
+    console.log("browser detected...");
+    // For browsers, use this syntax:
+    //  (nameOfDb, version number, description, db size)
+    // By default, set version to 1.0, and size to 2MB
+    db = window.openDatabase("superdb", "1.0", "Database for SuperHeros Rescue Agency app");
+  }
+  else {
+    alert("mobile device detected");
+    console.log("mobile device detected!");
+    var databaseDetails = {"name":"superdb.db", "location":"default"}
+    db = window.sqlitePlugin.openDatabase(databaseDetails);
+    console.log("done opening db");
+	alert("done opening db");
+
+	
+  }
+
+  if (!db) {
+    alert("databse not opened!");
+    return false;
+  }
+
+}
+
+
+// ERROR MESSAGES
+function onReadyTransaction( ){
+		console.log( 'Transaction completed' );
+		alert("Transaction completed");
+	}
+	function onSuccessExecuteSql( tx, results ){
+		console.log( 'Execute SQL completed' );
+	alert( "Execute SQL completed" );
+	 document.querySelector('#status').innerHTML = msg;
+	}
+	function onError( err ){
+		console.log( err )
+	}
+
+
+
+// CREATE DB
+db.transaction(
+		function(query){
+		query.executeSql(
+				"CREATE TABLE IF NOT EXISTS heros (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, isAvailable INTEGER)",
+				[],
+				onSuccessExecuteSql,
+				onError
+			)
+		},
+		onError,
+		onReadyTransaction
+	)
+
+
+
+
+
 document.getElementById("inhero").addEventListener("click", inserthero);
 document.getElementById("showhero").addEventListener("click", showhero);
 document.getElementById("rescue").addEventListener("click", rescue);
+
+	var name = document.getElementById("name").value;
+	var isav = document.getElementById("isav").value;
+	var msg ="inserted successfully"
 
 function inserthero() {
   // debug:
   console.log("insert button pressed!");
   alert("insert button pressed!");
+	
+db.transaction(
+		function(query){
+			query.executeSql( "INSERT INTO heros(name,isAvailable) VALUES(?)",
+			[name,isav],
+			onSuccessExecuteSql,
+			onError )
+		},
+		onError,
+		onReadyTransaction
+	)
+	
+	
 }
 
 function showhero() {
@@ -17,7 +103,7 @@ function showhero() {
 function rescue() {
   //debug:
   console.log("rescue button pressed!");
-  alert("show button pressed!");
+  alert("Rescue button pressed!");
 }
 
 
--- Final/www/js/index.js
+++ Final/www/js/index.js
@@ -1,7 +1,8 @@
 document.addEventListener("deviceready", connectToDatabase);
 
 
-var db = null;
+var db = null; 
+db = window.openDatabase("superdb", "1.0", "Database for SuperHeros Rescue Agency app", 2 * 1024 * 1024);
 
 function connectToDatabase() {
   console.log("device is ready - connecting to database");
@@ -11,12 +12,12 @@ function connectToDatabase() {
     // For browsers, use this syntax:
     //  (nameOfDb, version number, description, db size)
     // By default, set version to 1.0, and size to 2MB
-    db = window.openDatabase("superhero", "1.0", "Database for SuperHeros Rescue Agency app");
+    db = window.openDatabase("superdb", "1.0", "Database for SuperHeros Rescue Agency app");
   }
   else {
     alert("mobile device detected");
     console.log("mobile device detected!");
-    var databaseDetails = {"name":"superhero.db", "location":"default"}
+    var databaseDetails = {"name":"superdb.db", "location":"default"}
     db = window.sqlitePlugin.openDatabase(databaseDetails);
     console.log("done opening db");
 	alert("done opening db");
@@ -32,14 +33,65 @@ function connectToDatabase() {
 }
 
 
+// ERROR MESSAGES
+function onReadyTransaction( ){
+		console.log( 'Transaction completed' );
+		alert("Transaction completed");
+	}
+	function onSuccessExecuteSql( tx, results ){
+		console.log( 'Execute SQL completed' );
+	alert( "Execute SQL completed" );
+	 document.querySelector('#status').innerHTML = msg;
+	}
+	function onError( err ){
+		console.log( err )
+	}
+
+
+
+// CREATE DB
+db.transaction(
+		function(query){
+		query.executeSql(
+				"CREATE TABLE IF NOT EXISTS heros (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, isAvailable INTEGER)",
+				[],
+				onSuccessExecuteSql,
+				onError
+			)
+		},
+		onError,
+		onReadyTransaction
+	)
+
+
+
+
+
 document.getElementById("inhero").addEventListener("click", inserthero);
 document.getElementById("showhero").addEventListener("click", showhero);
 document.getElementById("rescue").addEventListener("click", rescue);
+
+	var name = document.getElementById("name").value;
+	var isav = document.getElementById("isav").value;
+	var msg ="inserted successfully"
 
 function inserthero() {
   // debug:
   console.log("insert button pressed!");
   alert("insert button pressed!");
+	
+db.transaction(
+		function(query){
+			query.executeSql( "INSERT INTO heros(name,isAvailable) VALUES(?)",
+			[name,isav],
+			onSuccessExecuteSql,
+			onError )
+		},
+		onError,
+		onReadyTransaction
+	)
+	
+	
 }
 
 function showhero() {
