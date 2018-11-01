288fd3f3786bb19781fbfdca623d8deb542d25f3
alaydesai094 <alaydesai094@gmail.com>
2018-11-01 14:01:04 -0400

    prank

---
+++ sound/platforms/browser/www/js/index.js
@@ -0,0 +1,17 @@
+ document.getElementById("dj").addEventListener("click",playSounddj);
+  document.getElementById("kl").addEventListener("click",playSoundkl);
+  document.getElementById("dong").addEventListener("click",playSounddong);
+ 
+ function playSounddj() {
+	 	alert("buttom pressed");
+         document.getElementById('djsound').play();
+      }
+ 
+ function playSoundkl() {
+	 	alert("buttom pressed");
+         document.getElementById('klsound').play();
+      }
+ function playSounddong() {
+	 	alert("buttom pressed");
+         document.getElementById('dongsound').play();
+      }
